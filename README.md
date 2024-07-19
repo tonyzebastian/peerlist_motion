037bafa2fd2577f9e603416c9c88479d109f7392
tonyzebaztian <126711013+tonyzebaztian@users.noreply.github.com>
2024-07-19 21:18:08 +0530

    first main commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# peerlist_motion
+A repo to show my explorations with Peerlist Interaction Design Challenge 2024
+Link - https://peerlist.io/blog/commentary/interaction-design-challenge
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        boxShadow: {
+            'custom-border': '0 0 0 8px #E2F5EA', 
+          },
+
+        colors: {
+          'custom-green-dark': '#00AA45',
+          'custom-green-medium': '#6FCF97', 
+          'custom-green-light': '#E2F5EA', 
+        },
+
+        fontFamily: {
+          'JetBrains': ['JetBrains Mono', 'monospace'], // Custom font
+        },
+        
+      },
+    },
+    plugins: [],
+  }
